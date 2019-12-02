19a47b59991efd904f1e07c58c8e7873e9c6985d
acary <info@andycary.com>
2019-12-02 14:35:40 -0600

    set up test for ml engine

--- controllers/ulmfit.py
+++ controllers/ulmfit.py
@@ -5,28 +5,30 @@ import pandas as pd
 import numpy as np
 from functools import partial
 
+
 def load_ulm_model():
     ''' Load ULM prediction model '''
 
     global model_ulmfit
     # `model_umlfit` variable refers to the global variable
 
-    with open('models/ulmfit_classifier_model.pkl', 'rb') as f:
-        model_ulmfit = pickle.load(f)
+    # with open('models/ulmfit_classifier_model.pkl', 'rb') as f:
+    #     model_ulmfit = pickle.load(f)
         # print(type(model_ulmfit))
+
 
 def uml_predict():
     ''' Make ULM prediction '''
     # print(model_ulmfit)
 
     predict_response = [{
-        "query":"What is the status of my loan application?",
-        "intent":"loan application",
-        "score":0.40
+        "query": "What is the status of my loan application?",
+        "intent": "loan application",
+        "score": 0.40
     }]
 
     meets_threshold = 0.70
-    probability = float(predict_response[0].get("score")) # 0.80
+    probability = float(predict_response[0].get("score"))  # 0.80
     if probability > meets_threshold:
         print(predict_response[0].get("intent"))
     else:
